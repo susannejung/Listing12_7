346413ae358807325534d6ee49829821fb1fde49
SUHA <suha@zealand.dk>
2023-11-28 09:41:43 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            CircleWithException c1 = new CircleWithException(5);
+            CircleWithException c2 = new CircleWithException(10);
+            CircleWithException c3 = new CircleWithException(-1);
+        }
+        catch (IllegalArgumentException ex) {
+            System.out.println(ex);
+        }
+
+        System.out.println("Number of objects created: " +
+                CircleWithException.getNumberOfObjects());
+    }
+}
